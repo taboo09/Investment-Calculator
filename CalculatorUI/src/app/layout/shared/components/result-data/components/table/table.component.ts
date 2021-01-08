bf7c776bb030fd4c62d506394715ab6baefff2c4
Marius <marius_taboo@yahoo.com>
2021-01-08 22:09:29 +0000

    visual reports

--- CalculatorUI/src/app/layout/shared/components/result-data/components/table/table.component.ts
+++ CalculatorUI/src/app/layout/shared/components/result-data/components/table/table.component.ts
@@ -1,15 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit {
+export class TableComponent implements OnInit, OnChanges {
+  dataSource!: MatTableDataSource<any>;
+
+  @Input() type: string = '';
+  @Input() data:any[] = [];
+
+  displayedColumns: string[] = ['index', 'startPrincipal', 'startBalance', 'interest', 'endBalance', 'endPrincipal'];
 
   constructor() { }
 
+  ngOnChanges(changes: SimpleChanges): void {
+    this.createColumns();
+
+    this.dataSource = new MatTableDataSource(this.data);
+  }
+
   ngOnInit(): void {
+    
+  }
+
+  createColumns(){
+    switch (this.type) {
+      case 'investment':
+        break;
+      
+      case 'interest':
+        if (this.displayedColumns[this.displayedColumns.length - 1] !== 'tax')
+          this.displayedColumns.push('tax');
+        break;
+
+      default:
+        break;
+    }
   }
 
 }
--- CalculatorUI/src/app/layout/shared/components/result-data/result-data.module.ts
+++ CalculatorUI/src/app/layout/shared/components/result-data/result-data.module.ts
@@ -4,6 +4,7 @@ import { ResultDataComponent } from './result-data.component';
 import { TableComponent } from './components/table/table.component';
 import { LineChartComponent } from './components/line-chart/line-chart.component';
 import { HighchartsChartModule } from 'highcharts-angular';
+import { MatTableModule } from '@angular/material/table';
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
@@ -11,6 +12,7 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     MatCardModule,
+    MatTableModule,
     HighchartsChartModule
   ],
   exports: [ResultDataComponent]
