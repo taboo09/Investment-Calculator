5276cd9782cc51f44a6822f61e4144e17e5fc00e
Marius <marius_taboo@yahoo.com>
2021-01-17 18:52:21 +0000

    container 4cols grid - globally

--- CalculatorUI/src/app/layout/history/history.component.ts
+++ CalculatorUI/src/app/layout/history/history.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history.component.scss']
 })
 export class HistoryComponent implements OnInit {
+  loading: boolean = false;
 
   constructor() { }
 
--- CalculatorUI/src/app/layout/history/history.module.ts
+++ CalculatorUI/src/app/layout/history/history.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { HistoryRoutingModule } from './history-routing.module';
 import { HistoryComponent } from './history.component';
+import { TitleModule } from '../shared/components/title/title.module';
+import { LoadingOverlayModule } from 'src/app/shared/modules/loading-overlay/loading-overlay.module';
 
 
 @NgModule({
   declarations: [HistoryComponent],
   imports: [
     CommonModule,
-    HistoryRoutingModule
+    HistoryRoutingModule,
+    TitleModule,
+    LoadingOverlayModule
   ]
 })
 export class HistoryModule { }
