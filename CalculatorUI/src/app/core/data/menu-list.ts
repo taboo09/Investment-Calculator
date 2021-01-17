fcfff0d77922a737360cfab49c735503d8cfd04a
Marius <marius_taboo@yahoo.com>
2021-01-17 14:10:25 +0000

    home completed

--- CalculatorUI/src/app/core/data/menu-list.ts
+++ CalculatorUI/src/app/core/data/menu-list.ts
@@ -1,8 +1,14 @@
 let menu_list = [
-    { icon: 'fa-home', title: 'Home', link: 'home', color: '#2ECC40' },
-    { icon: 'fa-tachometer-alt', title: 'Interest Calculator', link: 'interest', color: '#F012BE' },
-    { icon: 'fa-piggy-bank', title: 'Investment Calculator', link: 'investment', color: '#FF851B' },
-    { icon: 'fa-chart-bar', title: 'History data comparation', link: 'history', color: '#85144b' }
+    { index: 0, icon: 'fa-home', title: 'Home', link: 'home', color: '#2ECC40' },
+    { index: 1, icon: 'fa-tachometer-alt', title: 'Interest Calculator', link: 'interest', color: '#F012BE',
+        description: 'Interest Calculator can help determine the interest payments and final balances on not only fixed principal amounts, but also additional periodic contributions' 
+    },
+    { index: 2, icon: 'fa-piggy-bank', title: 'Investment Calculator', link: 'investment', color: '#FF851B',
+        description: 'The Investment Calculator can be used to calculate a specific parameter for an investment plan' 
+    },
+    { index: 3, icon: 'fa-chart-bar', title: 'History data comparation', link: 'history', color: '#85144b',
+        description: 'Data comparation used to project an investment based on historical values' 
+    }
 ];
 
 export default menu_list;
--- CalculatorUI/src/app/layout/home/home.component.ts
+++ CalculatorUI/src/app/layout/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import menu_list from 'src/app/core/data/menu-list';
+
+const title = 'investment calculator';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +9,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  menu: any;
+  font_size:number = 200;
+  title_arr: any = [];
+  selected_title = '';
+  selected_desc = '';
+
+  @HostListener('window:resize', ['$event'])
+  getScreenSize() {
+    let scrWidth = window.innerWidth;
+    
+    this.calculateFontSize(scrWidth);
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+    this.menu = menu_list.filter(x => x.link !== 'home');
+
+    this.setTitle(title);
+
+    this.selectItem(1);
+  }
+
+  calculateFontSize(screenWidth: number){
+    
+    if (screenWidth >= 1200) this.font_size = 300;
+    else if (screenWidth <= 600) this.font_size = 200; 
+    else {
+      let p = (screenWidth - 600) / 6;
+
+      this.font_size = 200 + p;
+    }
+
+  }
+
+  setTitle(title: string){
+    for (let i = 0; i < title.length; i++) {
+      this.title_arr.push({ char: title[i], font: 1 });
+    }
+
+    this.title_arr[0].font = 1.05;
+    this.title_arr[1].font = 1.5;
+    this.title_arr[2].font = 1.95;
+    this.title_arr[11].font = 1.95;
+    this.title_arr[12].font = 1.5;
+    this.title_arr[13].font = 1.05;
+
+    this.title_arr = this.title_arr.map((x: { font: number; char: string; }) => {
+      if(x.font > 1) {
+        return {
+          char: x.char.toUpperCase(),
+          font: x.font
+        };
+      }
+      return x;
+    });
+  }
+
+  selectItem(i: number){
+    this.selected_title = this.menu.find((x: { index: number; }) => x.index === i).title;
+    this.selected_desc = this.menu.find((x: { index: number; }) => x.index === i).description;
   }
 
 }
